1e3f9de9e059a2db19ca55aabc255367c42a534b
KateAiken <kate_aiken@icloud.com>
2026-02-05 16:44:16 -0500

    Feb 5 lab
    Sensors

--- Sharp203/Sharp203.ino
+++ Sharp203/Sharp203.ino
@@ -10,10 +10,14 @@
  */
 
 // Arduino analog input pin to which the Sharp sensor is connected
-const byte SHARP_PIN = A0;
+const byte SHARP_FRONT = A0;
+const byte SHARP_LEFT = A1;
+const byte SHARP_RIGHT = A2;
 
 // Variables to store the proximity measurement
-int sharp_val = 0; // integer read from analog pin
+int sharp_left = 0; // integer read from analog pin
+int sharp_right = 0; 
+int sharp_front = 0; 
 float sharp_range; // range measurement [cm]
 
 void setup()
@@ -25,10 +29,15 @@ void setup()
 void loop()
 {
     // Read the sensor output (0-1023, which is 10 bits and fits inside an Arduino int-type)
-    sharp_val = analogRead(SHARP_PIN);
-    sharp_val = 6950.1* pow(sharp_val, -1.034);
+    sharp_left = analogRead(SHARP_LEFT);
+    sharp_right = analogRead(SHARP_RIGHT);
+    sharp_front = analogRead(SHARP_FRONT);
+    int sharp_dist_front = 6950.1* pow(sharp_front, -1.034);
+    int sharp_dist_right = 9501.2 *pow(sharp_right, -1.088);
+    int sharp_dist_left = 10961 *pow(sharp_left, -1.121);
+    
     // Print all values
-    Serial.print(sharp_val);
+    Serial.print(sharp_dist_left);
     Serial.print("\n");
 
     // Delay for a bit before reading the sensor again
